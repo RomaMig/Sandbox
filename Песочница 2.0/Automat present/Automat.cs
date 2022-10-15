141da4dc5a5284cb743a03abfeab7b7d08f94e86
pc <pc@DESKTOP-HM4A4NJ>
2022-10-15 19:11:19 +0300

    Добавьте файлы проекта.

---
+++ Песочница 2.0/Automat present/Automat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Песочница_2._0.Automate_present
+{
+    class Automat
+    {
+
+    }
+
+    struct State
+    {
+
+    }
+
+    struct Transition
+    {
+
+    }
+}
